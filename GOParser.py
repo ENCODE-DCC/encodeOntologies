c9a1699f41a60e9122b17c48a71fe828d5799aac
nikhilRP <nikhilrp@stanford.edu>
2013-07-19 14:48:13 -0700

    Some logic to get slims for encode data

--- GOParser.py
+++ GOParser.py
@@ -1,7 +1,8 @@
 # Module downloads, parses all ontology files and indexes them in Elastic Search
 # Note: This is the basic version
-from pyelasticsearch import *
+from pyelasticsearch import ElasticSearch
 from urllib2 import Request, urlopen
+
 
 uberonURL = "http://purl.obolibrary.org/obo/uberon.obo"
 cellOntologyURL = "http://purl.obolibrary.org/obo/cl.obo"
@@ -70,18 +71,40 @@ def getAncestors(goid):
     return set(recursiveArray)
 
 
-def getRelatedTerms(goid):
-    ''' Get relatinos of GO ID'''
+def getRelatedTerms(goid, relationshipType):
+    ''' Get relations of GO ID'''
 
     recursiveArray = [goid]
     if terms.has_key(goid):
-        relations = terms[goid]['relations']
+        relations = terms[goid][relationshipType]
         if len(relations) > 0:
             for relation in relations:
-                recursiveArray.extend(getAncestors(relation))
+                recursiveArray.extend(getRelatedTerms(relation, relationshipType))
 
     return set(recursiveArray)
 
+
+def getSlims(goid):
+    ''' Get Slims '''
+
+    slims = []
+    slimTerms = {
+        'UBERON:0000383': 'musculature of body',
+        'UBERON:0000949': 'endocrine system',
+        'UBERON:0000990': 'reproductive system',
+        'UBERON:0001004': 'respiratory system',
+        'UBERON:0001007': 'digestive system',
+        'UBERON:0001008': 'excretory system',
+        'UBERON:0001009': 'circulatory system',
+        'UBERON:0001016': 'nervous system',
+        'UBERON:0001434': 'skeletal system',
+        'UBERON:0002405': 'immune system',
+        'UBERON:0002416': 'integumental system'
+    }
+    for slimTerm in slimTerms:
+        if slimTerm in terms[term]['closure']:
+            slims.append(slimTerms[slimTerm])
+    return slims
 
 #counter for indexing this is used as ID in elastic search
 counter = 0
@@ -90,6 +113,7 @@ print "Starting...."
 print
 
 terms = {}
+terms['UBERON:0001062'] = {'id': 'UBERON:0001062', 'name': 'anatomical entity', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': []}
 stupid_terms = []
 
 for url in urls:
@@ -109,47 +133,48 @@ for url in urls:
         #get the term using the two parsing functions
         term = parseTagValue(getTerm(oboFile))
         if len(term) != 0:
-            # Handling root terms
-            if term['id'][0] == 'UBERON:0000000':
-                terms[term['id'][0]] = {'id': '', 'name': '', 'parents': [], 'children': [], 'relations': [], 'closure': []}
-                terms[term['id'][0]]['id'] = term['id'][0]
-                terms[term['id'][0]]['name'] = term['name'][0]
-            else:
-                try:
-                    termID = term['id'][0]
-                    termName = term['name'][0]
+            if (term['id'][0]).find(':') != -1:
+                # Handling root terms
+                if term['id'][0] == 'UBERON:0000000':
+                    terms[term['id'][0]] = {'id': '', 'name': '', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': [], 'slims': []}
+                    terms[term['id'][0]]['id'] = term['id'][0]
+                    terms[term['id'][0]]['name'] = term['name'][0]
+                else:
+                    try:
+                        termID = term['id'][0]
+                        termName = term['name'][0]
 
-                    if term.has_key('is_a'):
-                        termParents = [p.split()[0] for p in term['is_a']]
+                        if term.has_key('is_a'):
+                            termParents = [p.split()[0] for p in term['is_a']]
 
-                        if not terms.has_key(termID):
-                            #each goid will have two arrays of parents and children
-                            terms[termID] = {'id': '', 'name': '', 'parents': [], 'children': [], 'relations': [], 'closure': []}
+                            if not terms.has_key(termID):
+                                #each goid will have two arrays of parents and children
+                                terms[termID] = {'id': '', 'name': '', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': [], 'slims': []}
 
-                        #append termID and termName to the dict
-                        terms[termID]['id'] = termID
-                        terms[termID]['name'] = termName
+                            #append termID and termName to the dict
+                            terms[termID]['id'] = termID
+                            terms[termID]['name'] = termName
 
-                        #append parents of the current term
-                        terms[termID]['parents'] = termParents
+                            #append parents of the current term
+                            terms[termID]['parents'] = termParents
 
-                        #for every parent term, add this current term as children
-                        for termParent in termParents:
-                            if not terms.has_key(termParent):
-                                terms[termParent] = {'parents': [], 'children': []}
-                            terms[termParent]['children'].append(termID)
-                    if term.has_key('relationship'):
-                        relations = [p.split()[0] for p in term['relationship']]
-                        relationTerms = [p.split()[1] for p in term['relationship']]
-                        count = 0
-                        relationships = ['part_of', 'develops_from']
-                        for relation in relations:
-                            if relation in relationships:
-                                terms[termID]['relations'].append(relationTerms[count])
-                            count = count + 1
+                            #for every parent term, add this current term as children
+                            for termParent in termParents:
+                                if not terms.has_key(termParent):
+                                    terms[termParent] = {'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': [], 'slims': []}
+                                terms[termParent]['children'].append(termID)
+                            if term.has_key('relationship'):
+                                relations = [p.split()[0] for p in term['relationship']]
+                                relationTerms = [p.split()[1] for p in term['relationship']]
+                                count = 0
+                                relationships = ['part_of', 'develops_from', 'overlaps']
+                                for relation in relations:
+                                    if relation in relationships:
+                                        terms[termID][relation].append(relationTerms[count])
+                                    count = count + 1
 
-                except KeyError:
-                    stupid_terms.append(termID)
+                    except KeyError:
+                        stupid_terms.append(termID)
         else:
             break
 
@@ -157,23 +182,58 @@ for url in urls:
 for stupid_term in stupid_terms:
     del(terms[stupid_term])
 
-# Generating closure from ancestors and relations
+useless = []
 for term in terms:
     try:
-        ancestors = getAncestors(terms[term]['id'])
-        for ancestor in ancestors:
-            terms[term]['closure'].append(ancestor)
-        relations = getRelatedTerms(terms[term]['id'])
-        for relation in relations:
-            terms[term]['closure'].append(relation)
-    except KeyError:
-        pass
+        terms[term]['id']
+    except:
+        useless.append(term)
 
-print "Indexing the data in ElasticSearch..."
+for useL in useless:
+    del(terms[useL])
+
+print "Take a break, I have to calculate closures for " + str(len(terms)) + " ontology terms and index them in elasticsearch. Long operation. Sigh!!"
+
 for term in terms:
-    counter += 1
-    connection.index(index_name, doc_type_name, terms[term], id=counter)
+
+    # Get relationship terms and append to closure
+    rels = getRelatedTerms(term, 'part_of')
+    for rel in rels:
+        terms[term]['closure'].append(rel)
+
+    terms[term]['closure'] = list(set(terms[term]['closure']))
+    devs = getRelatedTerms(term, 'develops_from')
+    for dev in devs:
+        terms[term]['closure'].append(dev)
+
+    terms[term]['closure'] = list(set(terms[term]['closure']))
+    overs = getRelatedTerms(term, 'overlaps')
+    for over in overs:
+        terms[term]['closure'].append(over)
+
+    terms[term]['closure'] = list(set(terms[term]['closure']))
+    ancestors = getAncestors(terms[term]['id'])
+    for ancestor in ancestors:
+        terms[term]['closure'].append(ancestor)
+        nodes = getRelatedTerms(ancestor, 'part_of')
+        for node in nodes:
+            terms[term]['closure'].append(node)
+
+    terms[term]['closure'] = list(set(terms[term]['closure']))
+    terms[term]['closure'].remove(term)
+    terms[term]['slims'] = getSlims(term)
+
+count = 0
+for term in terms:
+    del(terms[term]['children'])
+    del(terms[term]['part_of'])
+    del(terms[term]['develops_from'])
+    del(terms[term]['overlaps'])
+    del(terms[term]['parents'])
+    # import pdb; pdb.set_trace();
+    connection.index(index_name, doc_type_name, terms[term], id=count)
     connection.refresh()
+    count = count + 1
 
 print
-print "Total GO Terms indexed " + str(counter)
+print "Total GO Terms indexed " + str(count)
