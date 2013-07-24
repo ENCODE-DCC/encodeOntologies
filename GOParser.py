0d5b8abf942ddac798763f3021591becaa141b84
nikhilRP <Nikhil@angus.local>
2013-07-23 22:24:51 -0700

    Added organ and organ system slims

--- GOParser.py
+++ GOParser.py
@@ -71,20 +71,20 @@ def getAncestors(goid):
     return set(recursiveArray)
 
 
-def getRelatedTerms(goid, relationshipType):
+def getRelatedTerms(goid):
     ''' Get relations of GO ID'''
 
     recursiveArray = [goid]
     if terms.has_key(goid):
-        relations = terms[goid][relationshipType]
+        relations = terms[goid]['data']
         if len(relations) > 0:
             for relation in relations:
-                recursiveArray.extend(getRelatedTerms(relation, relationshipType))
+                recursiveArray.extend(getRelatedTerms(relation))
 
     return set(recursiveArray)
 
 
-def getSlims(goid):
+def getSystemSlims(goid):
     ''' Get Slims '''
 
     slims = []
@@ -106,6 +106,43 @@ def getSlims(goid):
             slims.append(slimTerms[slimTerm])
     return slims
 
+
+def getOrganSlims(goid):
+    ''' Get Organ Slims '''
+
+    slims = []
+    slimTerms = {
+        'UBERON:0000004': 'nose',
+        'UBERON:0000056': 'ureter',
+        'UBERON:0000057': 'urethra',
+        'UBERON:0000059': 'large intestine',
+        'UBERON:0000165': 'mouth',
+        'UBERON:0000945': 'stomach',
+        'UBERON:0000948': 'heart',
+        'UBERON:0000955': 'brain',
+        'UBERON:0000970': 'eye',
+        'UBERON:0000991': 'gonad',
+        'UBERON:0001043': 'esophagus', 
+        'UBERON:0001255': 'urinay bladder',
+        'UBERON:0001264': 'pancreas',
+        'UBERON:0001474': 'bone element',
+        'UBERON:0002003': 'peripheral nerve',
+        'UBERON:0002048': 'lung',
+        'UBERON:0002097': 'skin of body',
+        'UBERON:0002107': 'liver',
+        'UBERON:0002108': 'small intestine',
+        'UBERON:0002113': 'kidney',
+        'UBERON:0002240': 'spinal cord',
+        'UBERON:0002367': 'prostate gland',
+        'UBERON:0002370': 'thymus',
+        'UBERON:0003126': 'trachea'
+    }
+    for slimTerm in slimTerms:
+        if slimTerm in terms[term]['closure']:
+            slims.append(slimTerms[slimTerm])
+    return slims
+
+
 #counter for indexing this is used as ID in elastic search
 counter = 0
 
@@ -113,8 +150,9 @@ print "Starting...."
 print
 
 terms = {}
+term_queue = {}
 # UBERON has 2 Roots god knows why, so one of the roots is entered manually here
-terms['UBERON:0001062'] = {'id': 'UBERON:0001062', 'name': 'anatomical entity', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': []}
+terms['UBERON:0001062'] = {'id': 'UBERON:0001062', 'name': 'anatomical entity', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': [], 'data': []}
 stupid_terms = []
 
 for url in urls:
@@ -137,7 +175,7 @@ for url in urls:
             if (term['id'][0]).find(':') != -1:
                 # Handling root terms
                 if term['id'][0] == 'UBERON:0000000':
-                    terms[term['id'][0]] = {'id': '', 'name': '', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': [], 'slims': []}
+                    terms[term['id'][0]] = {'id': '', 'name': '', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'organs': [], 'closure': [], 'slims': [], 'data': []}
                     terms[term['id'][0]]['id'] = term['id'][0]
                     terms[term['id'][0]]['name'] = term['name'][0]
                 else:
@@ -150,7 +188,7 @@ for url in urls:
 
                             if not terms.has_key(termID):
                                 #each goid will have two arrays of parents and children
-                                terms[termID] = {'id': '', 'name': '', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': [], 'slims': []}
+                                terms[termID] = {'id': '', 'name': '', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'organs': [], 'closure': [], 'slims': [], 'data': []}
 
                             #append termID and termName to the dict
                             terms[termID]['id'] = termID
@@ -162,13 +200,13 @@ for url in urls:
                             #for every parent term, add this current term as children
                             for termParent in termParents:
                                 if not terms.has_key(termParent):
-                                    terms[termParent] = {'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': [], 'slims': []}
+                                    terms[termParent] = {'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'organs': [], 'closure': [], 'slims': [], 'data': []}
                                 terms[termParent]['children'].append(termID)
                             if term.has_key('relationship'):
                                 relations = [p.split()[0] for p in term['relationship']]
                                 relationTerms = [p.split()[1] for p in term['relationship']]
                                 count = 0
-                                relationships = ['part_of', 'develops_from', 'overlaps']
+                                relationships = ['part_of', 'develops_from']
                                 for relation in relations:
                                     if relation in relationships:
                                         terms[termID][relation].append(relationTerms[count])
@@ -196,43 +234,27 @@ for useL in useless:
 print "Take a break, I have to calculate closures for " + str(len(terms)) + " ontology terms and index them in elasticsearch. Long operation. Sigh!!"
 
 for term in terms:
+    terms[term]['data'] = list(set(terms[term]['parents']) | set(terms[term]['part_of']) | set(terms[term]['develops_from']))
 
-    # Get relationship terms and append to closure
-    rels = getRelatedTerms(term, 'part_of')
-    for rel in rels:
-        words = getAncestors(rel)
+
+for term in terms:
+    for t in terms[term]['data']:
+        words = getRelatedTerms(t)
         for word in words:
             terms[term]['closure'].append(word)
 
     terms[term]['closure'] = list(set(terms[term]['closure']))
-    devs = getRelatedTerms(term, 'develops_from')
-    for dev in devs:
-        terms[term]['closure'].append(dev)
+    terms[term]['systems'] = getSystemSlims(term)
+    terms[term]['organs'] = getOrganSlims(term)
 
-    terms[term]['closure'] = list(set(terms[term]['closure']))
-    overs = getRelatedTerms(term, 'overlaps')
-    for over in overs:
-        terms[term]['closure'].append(over)
-
-    terms[term]['closure'] = list(set(terms[term]['closure']))
-    ancestors = getAncestors(terms[term]['id'])
-    for ancestor in ancestors:
-        terms[term]['closure'].append(ancestor)
-        nodes = getRelatedTerms(ancestor, 'part_of')
-        for node in nodes:
-            terms[term]['closure'].append(node)
-
-    terms[term]['closure'] = list(set(terms[term]['closure']))
-    terms[term]['closure'].remove(term)
-    terms[term]['slims'] = getSlims(term)
 
 count = 0
 for term in terms:
     del(terms[term]['children'])
     del(terms[term]['part_of'])
     del(terms[term]['develops_from'])
-    del(terms[term]['overlaps'])
     del(terms[term]['parents'])
+    del(terms[term]['data'])
     # import pdb; pdb.set_trace();
     connection.index(index_name, doc_type_name, terms[term], id=count)
     connection.refresh()
