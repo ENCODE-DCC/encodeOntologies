4d9c9c727ecf99747b336de4e3d8e2e31c231389
nikhilRP <nikhilrp@stanford.edu>
2013-07-26 13:21:07 -0700

    improvement to the parser. Added merged ontology

--- GOParser.py
+++ GOParser.py
@@ -2,12 +2,9 @@
 # Note: This is the basic version
 from pyelasticsearch import ElasticSearch
 from urllib2 import Request, urlopen
+from collections import OrderedDict
 
-
-uberonURL = "http://purl.obolibrary.org/obo/uberon.obo"
-cellOntologyURL = "http://purl.obolibrary.org/obo/cl.obo"
-obiURL = "http://purl.obolibrary.org/obo/obi/obi.obo"
-
+uberonURL = "http://svn.code.sf.net/p/obo/svn/uberon/trunk/merged.obo"
 urls = [uberonURL]
 
 #make connection to elastic search
@@ -16,6 +13,8 @@ connection = ElasticSearch('http://localhost:9200')
 index_name = "ontology"
 doc_type_name = "basic"
 
+import sys
+sys.setrecursionlimit(20000)
 
 def getTerm(stream):
     ''' Get GO term block from the obo file'''
@@ -71,17 +70,23 @@ def getAncestors(goid):
     return set(recursiveArray)
 
 
-def getRelatedTerms(goid):
-    ''' Get relations of GO ID'''
-
-    recursiveArray = [goid]
-    if terms.has_key(goid):
-        relations = terms[goid]['data']
-        if len(relations) > 0:
-            for relation in relations:
-                recursiveArray.extend(getRelatedTerms(relation))
-
-    return set(recursiveArray)
+def iterativeChildren(nodes):
+    results = []
+    print "Calulating closure ..."
+    while 1:
+        newNodes = []
+        if len(nodes) == 0:
+            print "****** Done *******"
+            print
+            break
+        for node in nodes:
+            results.append(node)
+            if terms[node]['data']:
+                for child in terms[node]['data']:
+                    if child not in results:
+                        newNodes.append(child)
+        nodes = newNodes
+    return results
 
 
 def getSystemSlims(goid):
@@ -112,6 +117,11 @@ def getOrganSlims(goid):
 
     slims = []
     slimTerms = {
+        'UBERON:0002369': 'adrenal gland',
+        'UBERON:0002110': 'gallbladder',
+        'UBERON:0002106': 'spleen',
+        'UBERON:0001173': 'billary tree',
+        'UBERON:0001043': 'esophagus',
         'UBERON:0000004': 'nose',
         'UBERON:0000056': 'ureter',
         'UBERON:0000057': 'urethra',
@@ -122,7 +132,7 @@ def getOrganSlims(goid):
         'UBERON:0000955': 'brain',
         'UBERON:0000970': 'eye',
         'UBERON:0000991': 'gonad',
-        'UBERON:0001043': 'esophagus', 
+        'UBERON:0001043': 'esophagus',
         'UBERON:0001255': 'urinay bladder',
         'UBERON:0001264': 'pancreas',
         'UBERON:0001474': 'bone element',
@@ -130,12 +140,34 @@ def getOrganSlims(goid):
         'UBERON:0002048': 'lung',
         'UBERON:0002097': 'skin of body',
         'UBERON:0002107': 'liver',
+        'UBERON:0000059': 'large intestine',
         'UBERON:0002108': 'small intestine',
         'UBERON:0002113': 'kidney',
         'UBERON:0002240': 'spinal cord',
         'UBERON:0002367': 'prostate gland',
         'UBERON:0002370': 'thymus',
-        'UBERON:0003126': 'trachea'
+        'UBERON:0003126': 'trachea',
+        'UBERON:0001723': 'tongue',
+        'UBERON:0001737': 'larynx',
+        'UBERON:0006562': 'pharynx',
+        'UBERON:0001103': 'diaphragm',
+        'UBERON:0002185': 'bronchus',
+        'UBERON:0000029': 'lymph node',
+        'UBERON:0002391': 'lymph',
+        'UBERON:0010133': 'neuroendocrine gland',
+        'UBERON:0001132': 'parathyroid gland',
+        'UBERON:0002046': 'thyroid gland',
+        'UBERON:0001981': 'blood vessel',
+        'UBERON:0001473': 'lymphatic vessel',
+        'UBERON:0000178': 'blood',
+        'UBERON:0002268': 'olfactory organ',
+        'UBERON:0007844': 'cartilage element',
+        'UBERON:0001690': 'ear',
+        'UBERON:0001987': 'placenta',
+        'UBERON:0001911': 'mammary gland',
+        'UBERON:0001630': 'muscle organ',
+        'UBERON:0000007': 'pituitary gland',
+        'UBERON:0002370': 'thymus'
     }
     for slimTerm in slimTerms:
         if slimTerm in terms[term]['closure']:
@@ -149,10 +181,8 @@ counter = 0
 print "Starting...."
 print
 
-terms = {}
-term_queue = {}
-# UBERON has 2 Roots god knows why, so one of the roots is entered manually here
-terms['UBERON:0001062'] = {'id': 'UBERON:0001062', 'name': 'anatomical entity', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'overlaps': [], 'closure': [], 'data': []}
+terms = OrderedDict()
+root_terms = ['UBERON:0000000', 'UBERON:0001062', 'CL:0000000', 'CHEBI:50906', 'CHEBI:24431', 'CHEBI:36342', 'GO:0003674', 'GO:0005575', 'GO:0008150', 'IAO:0000030', 'PATO:0000001', 'PR:000018263', 'NCBITaxon:1']
 stupid_terms = []
 
 for url in urls:
@@ -173,47 +203,51 @@ for url in urls:
         term = parseTagValue(getTerm(oboFile))
         if len(term) != 0:
             if (term['id'][0]).find(':') != -1:
-                # Handling root terms
-                if term['id'][0] == 'UBERON:0000000':
-                    terms[term['id'][0]] = {'id': '', 'name': '', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'organs': [], 'closure': [], 'slims': [], 'data': []}
-                    terms[term['id'][0]]['id'] = term['id'][0]
-                    terms[term['id'][0]]['name'] = term['name'][0]
-                else:
-                    try:
-                        termID = term['id'][0]
-                        termName = term['name'][0]
+                try:
+                    termID = term['id'][0]
+                    termName = term['name'][0]
 
-                        if term.has_key('is_a'):
-                            termParents = [p.split()[0] for p in term['is_a']]
+                    if term.has_key('is_a'):
+                        termParents = [p.split()[0] for p in term['is_a']]
 
-                            if not terms.has_key(termID):
-                                #each goid will have two arrays of parents and children
+                        if not terms.has_key(termID):
+                            #each goid will have two arrays of parents and children
+                            terms[termID] = {'id': '', 'name': '', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'organs': [], 'closure': [], 'slims': [], 'data': []}
+
+                        #append termID and termName to the dict
+                        terms[termID]['id'] = termID
+                        terms[termID]['name'] = termName
+
+                        #append parents of the current term
+                        terms[termID]['parents'] = termParents
+
+                        #for every parent term, add this current term as children
+                        for termParent in termParents:
+                            if not terms.has_key(termParent):
+                                terms[termParent] = {'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'organs': [], 'closure': [], 'slims': [], 'data': []}
+                            terms[termParent]['children'].append(termID)
+                        if term.has_key('relationship'):
+                            relations = [p.split()[0] for p in term['relationship']]
+                            relationTerms = [p.split()[1] for p in term['relationship']]
+                            count = 0
+                            relationships = ['part_of', 'develops_from']
+                            for relation in relations:
+                                if relation in relationships:
+                                    terms[termID][relation].append(relationTerms[count])
+                                count = count + 1
+                    else:
+                        # Handling root terms for the uberon merged ontology
+                        if term['id'][0] in root_terms:
+                            if term['id'][0] not in terms:
                                 terms[termID] = {'id': '', 'name': '', 'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'organs': [], 'closure': [], 'slims': [], 'data': []}
-
-                            #append termID and termName to the dict
-                            terms[termID]['id'] = termID
-                            terms[termID]['name'] = termName
-
-                            #append parents of the current term
-                            terms[termID]['parents'] = termParents
-
-                            #for every parent term, add this current term as children
-                            for termParent in termParents:
-                                if not terms.has_key(termParent):
-                                    terms[termParent] = {'parents': [], 'children': [], 'part_of': [], 'develops_from': [], 'organs': [], 'closure': [], 'slims': [], 'data': []}
-                                terms[termParent]['children'].append(termID)
-                            if term.has_key('relationship'):
-                                relations = [p.split()[0] for p in term['relationship']]
-                                relationTerms = [p.split()[1] for p in term['relationship']]
-                                count = 0
-                                relationships = ['part_of', 'develops_from']
-                                for relation in relations:
-                                    if relation in relationships:
-                                        terms[termID][relation].append(relationTerms[count])
-                                    count = count + 1
-
-                    except KeyError:
-                        stupid_terms.append(termID)
+                                #append termID and termName to the dict
+                                terms[termID]['id'] = termID
+                                terms[termID]['name'] = termName
+                            else:
+                                terms[termID]['id'] = termID
+                                terms[termID]['name'] = termName
+                except KeyError:
+                    stupid_terms.append(termID)
         else:
             break
 
@@ -238,15 +272,14 @@ for term in terms:
 
 
 for term in terms:
-    for t in terms[term]['data']:
-        words = getRelatedTerms(t)
-        for word in words:
-            terms[term]['closure'].append(word)
+    print term
+    words = iterativeChildren(terms[term]['data'])
+    for word in words:
+        terms[term]['closure'].append(word)
 
     terms[term]['closure'] = list(set(terms[term]['closure']))
     terms[term]['systems'] = getSystemSlims(term)
     terms[term]['organs'] = getOrganSlims(term)
-
 
 count = 0
 for term in terms:
